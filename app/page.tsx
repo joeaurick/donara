59703299e0781f6e80d0fc7a048055259840f3c5
joeaurick <joeauricktube0101@gmail.com>
2026-07-08 04:47:44 +0700

    Test homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+import LocalBusinessSchema from "./components/seo/LocalBusinessSchema";
+import ProductSchema from "./components/seo/ProductSchema";
+import ReviewSchema from "./components/seo/ReviewSchema";
+import OrganizationSchema from "./components/seo/OrganizationSchema";
+import WebsiteSchema from "./components/seo/WebsiteSchema";
+import SearchActionSchema from "./components/seo/SearchActionSchema";
+import BreadcrumbSchema from "./components/seo/BreadcrumbSchema";
+import FAQSchema from "./components/seo/FAQSchema";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import WhyChoose from "./components/WhyChoose";
+import Gallery from "./components/Gallery";
+import Review from "./components/Review";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
 export default function Home() {
-  return <h1>TEST</h1>;
+  return (
+    <>
+      <LocalBusinessSchema />
+      <ProductSchema />
+      <ReviewSchema />
+      <OrganizationSchema />
+      <WebsiteSchema />
+      <SearchActionSchema />
+      <BreadcrumbSchema />
+      <FAQSchema />
+      <Navbar />
+      <Hero />
+      <About />
+      <Menu />
+    </>
+  );
 }
